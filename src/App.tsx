a27c2b4a2a52e1510c48a8421ca893b118798a43
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-07-02 14:46:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function App() {
+  const Wrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    padding: 8px;
+  `;
+
+  return (
+    <Wrapper>
+      <h1>App</h1>
+    </Wrapper>
+  );
+}
+
+export default App;
